65f22500a10d0ceecff4d3c4abb20c267683a568
Andre St-Louis <as87321234@gmail.com>
2020-05-06 19:30:13 -0400

    Add  hashing byte[]

--- src/main/java/com/pointtomap/iclassify/jpa/util/HashUtil.java
+++ src/main/java/com/pointtomap/iclassify/jpa/util/HashUtil.java
@@ -8,15 +8,40 @@ import org.apache.commons.codec.binary.Hex;
 
 public class HashUtil {
 
-	public static String sha256(String passwd) throws NoSuchAlgorithmException {
+	public static String sha256(String passwd) {
 
-		final MessageDigest digest = MessageDigest.getInstance("SHA-256");
-		final byte[] hashbytes = digest.digest(passwd.getBytes(StandardCharsets.UTF_8));
+		MessageDigest digest;
+		try {
+			digest = MessageDigest.getInstance("SHA-256");
+			final byte[] hashbytes = digest.digest(passwd.getBytes(StandardCharsets.UTF_8));
 
-		String sha_256hex = Hex.encodeHexString(hashbytes);
+			String sha_256hex = Hex.encodeHexString(hashbytes);
 
-		return sha_256hex;
+			return sha_256hex;
 
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null;
 	}
 
+	public static String sha256(byte[] byteArray) {
+
+		MessageDigest digest;
+		try {
+			digest = MessageDigest.getInstance("SHA-256");
+			final byte[] hashbytes = digest.digest(byteArray);
+
+			String sha_256hex = Hex.encodeHexString(hashbytes);
+
+			return sha_256hex;
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null;
+	}
 }
