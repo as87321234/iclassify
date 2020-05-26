029b6925e8c4f78d9c306f891bf1c9098edc93a6
Unknown <as87321234@gmail.com>
2020-05-25 21:56:30 -0400

    New ORM Classes

---
+++ src/main/java/com/pointtomap/iclassify/jpa/orm/IcWordTranslation.java
@@ -0,0 +1,84 @@
+package com.pointtomap.iclassify.jpa.orm;
+
+import java.io.Serializable;
+import java.util.Set;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.NamedQueries;
+import org.hibernate.annotations.NamedQuery;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.pointtomap.iclassify.jpa.util.ReflectionUtils;
+
+import lombok.ToString;
+
+/**
+ * The persistent class for the ic_user database table.
+ *
+ */
+@Entity
+@Table(name = "ic_word_translation")
+@EnableJpaRepositories
+@NamedQueries({
+
+		@NamedQuery(name = IcWordTranslation.FIND_ALL, query = "SELECT e FROM IcWordTranslation e")
+
+//		@NamedQuery(cacheable = false, readOnly = false, name = IcWordTranslation.FIND_USER_BY_USER_ID, query = "SELECT c FROM IcUser c where c.userId = :userId and c.deleted=false "),
+//
+//		@NamedQuery(cacheable = false, readOnly = false, name = IcWordTranslation.FIND_ALL_ACTIVE_USERS, query = "SELECT c FROM IcUser c where c.deleted = 'false' order by lower(c.username) asc "),
+//
+//		@NamedQuery(cacheable = false, readOnly = false, name = IcWordTranslation.FIND_USER_BY_USERNAME, query = "SELECT c FROM IcUser c where c.username = :username and c.deleted = 'false' ") 
+})
+
+@ToString
+public class IcWordTranslation extends IcEntity implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	public static final String FIND_ALL = "IcWordTranslation.findAll";
+//	public static final String FIND_USER_BY_USER_ID = "IcWordTranslation.findByWordTranslationid";
+//
+//	public static final String FIND_ALL_ACTIVE_USERS = "IcWordTranslation.findAllActiveUsers";
+//
+//	public static final String FIND_USER_BY_USERNAME = "IcWordTranslation.findUserByUsername";
+
+	@Override
+	public void clearKey() {
+		super.clearKey();
+		this.wordTranslationId = null;
+
+	}
+
+	@Id
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	@Column(name = "word_translation_id", updatable = false, nullable = false)
+	private UUID wordTranslationId;
+
+	private String defaultLanguage;
+
+	private String french;
+
+	private String spannish;
+
+	private String english;
+
+	@OneToMany(mappedBy = "IcWordUsage")
+	Set<IcWordUsage> icWordUsage;
+
+	public IcWordTranslation() {
+	}
+
+	public IcWordTranslation(final IcWordTranslation icWordTranslation) {
+
+		ReflectionUtils.cloneSkipNull(icWordTranslation, this, null);
+	}
+
+}
---
+++ src/main/java/com/pointtomap/iclassify/jpa/orm/IcWordUsage.java
@@ -0,0 +1,75 @@
+package com.pointtomap.iclassify.jpa.orm;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.NamedQueries;
+import org.hibernate.annotations.NamedQuery;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.pointtomap.iclassify.jpa.util.ReflectionUtils;
+
+import lombok.ToString;
+
+/**
+ * The persistent class for the ic_user database table.
+ *
+ */
+@Entity
+@Table(name = "ic_word_usage")
+@EnableJpaRepositories
+@NamedQueries({
+
+		@NamedQuery(name = IcWordUsage.FIND_ALL, query = "SELECT e FROM IcWordUsage e")
+
+//		@NamedQuery(cacheable = false, readOnly = false, name = IcWordUsage.FIND_USER_BY_USER_ID, query = "SELECT c FROM IcUser c where c.userId = :userId and c.deleted=false "),
+//
+//		@NamedQuery(cacheable = false, readOnly = false, name = IcWordUsage.FIND_ALL_ACTIVE_USERS, query = "SELECT c FROM IcUser c where c.deleted = 'false' order by lower(c.username) asc "),
+//
+//		@NamedQuery(cacheable = false, readOnly = false, name = IcWordUsage.FIND_USER_BY_USERNAME, query = "SELECT c FROM IcUser c where c.username = :username and c.deleted = 'false' ")
+})
+
+@ToString
+public class IcWordUsage extends IcEntity implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	public static final String FIND_ALL = "IcWordUsage.findAll";
+//	public static final String FIND_USER_BY_USER_ID = "IcUser.findByUserid";
+//
+//	public static final String FIND_ALL_ACTIVE_USERS = "IcUser.findAllActiveUsers";
+//
+//	public static final String FIND_USER_BY_USERNAME = "IcUser.findUserByUsername";
+
+	@Override
+	public void clearKey() {
+		super.clearKey();
+		this.wordUsageId = null;
+
+	}
+
+	@Id
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	@Column(name = "word_usage_id", updatable = false, nullable = false)
+	private UUID wordUsageId;
+
+	private IcWordUsageTypeEnum wordUsageTypeEnum;
+
+	private String exmaple;
+
+	public IcWordUsage() {
+	}
+
+	public IcWordUsage(final IcWordUsage wordUsageId) {
+
+		ReflectionUtils.cloneSkipNull(wordUsageId, this, null);
+	}
+
+}
